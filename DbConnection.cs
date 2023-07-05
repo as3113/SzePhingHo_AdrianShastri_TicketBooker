afc205a6df1ba4a72be5251191e6e08b711edd58
spho <hoszephing@gmail.com>
2023-07-05 11:01:08 -0400

    phase II testing

---
+++ DbConnection.cs
@@ -0,0 +1,43 @@
+﻿using System.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TicketTest
+{
+    internal static class DbConnection
+    {
+        // Use Lazy<T> to initialize database connection when it is first accessed
+        private static readonly Lazy<SqlConnection> lazyConnection = new Lazy<SqlConnection>(() =>
+        {
+            SqlConnection connection = new SqlConnection(getConnectionString());
+            connection.Open();
+            return connection; // return single instance of connection
+        });
+
+        // Method to construct and return the connection string for SQL server database connection
+        private static string getConnectionString()
+        {
+            // Define the values of server host, database name, username, and password.  
+            // Values maybe different for different individual's system
+            string server = "Server=localhost;";
+            string dbName = "Database=vanierAECWinter2023;";
+            string username = "User=sa;";
+            string password = "Password=1234;"; // key in your SQL server password
+
+            // Combine the strings above database connection string
+            string connectionString = string.Format("{0}{1}{2}{3}", server, dbName, username, password);
+            return connectionString;
+        }
+        public static SqlConnection Connection => lazyConnection.Value; // access to the lazily initialized database connection
+
+        // Method to be used in other classes for single database connection
+        public static SqlCommand CreateCommand()
+        {
+            return Connection.CreateCommand();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TicketTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void tbUsername_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            textBox.Text = "";
+        }
+
+        private void tbPassword_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            textBox.Text = "";
+        }
+
+
+        private void btnSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string username = tbUsername.Text;
+                string passwordBox = passBoxAdminSign.Password;
+
+                MessageBox.Show(passwordBox);
+
+                using (var cmd = DbConnection.CreateCommand())
+                {
+                    string query = $"SELECT * FROM adminaccount WHERE username = @username AND password = @password";
+                    cmd.CommandText = query;
+                    cmd.Parameters.AddWithValue("@username", username);
+                    cmd.Parameters.AddWithValue("@password", passwordBox);
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            // Authentication successful, open the new window or perform any desired action
+                            // For example, you can create a new instance of the window and show it
+                            AdminDashboard adminDashboard = new AdminDashboard();
+                            adminDashboard.Show();
+                        }
+                        else
+                        {
+                            // Authentication failed, display an error message
+                            MessageBox.Show("Invalid username or password.");
+                        }
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            AdminRegister adminRegister = new AdminRegister();
+            adminRegister.Show();
+        }
+
+        private void PasswordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            placeholderText.Visibility = Visibility.Collapsed;
+        }
+
+        private void PasswordBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(passBoxAdminSign.Password))
+                placeholderText.Visibility = Visibility.Visible;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+      
+
+    }
+}
